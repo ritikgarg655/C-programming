7a479125702d6d845d1856f03c17fc8b5ecb2852
ritikgarg655 <43903609+ritikgarg655@users.noreply.github.com>
2019-01-31 00:14:15 +0530

    Update code1.c
    done

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -1,4 +1,8 @@
 int max (int num1, int num2) {
+  if (num1>num2)
+    return num1;
+  else 
+    return num2;
   //check if num1 is greater than num2
     //if so, your answer is num1
     //otherwise, your answer is num2
